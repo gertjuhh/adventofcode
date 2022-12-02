439772e3cc938118c7d612b8850cbb42a14ce1a3
Geert Broekmans <geert@myonlinestore.com>
2022-12-02 21:33:38 +0100

    Day 1

---
+++ 2022/01/part1-2.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+$elves = array_map(
+    static fn ($in): int => array_sum(explode("\n", $in)),
+    preg_split('/^$/m', file_get_contents('input.txt'))
+);
+
+rsort($elves);
+
+printf('Elf with the most calories: %s%s', $elves[0], "\n");
+printf('Top 3 callories combined: %s%s', array_sum(array_slice($elves, 0, 3)), "\n");
